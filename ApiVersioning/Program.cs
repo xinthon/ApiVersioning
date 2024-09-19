d44a3e25ebc421de07077729d2525e92e3e10313
Sinthon Seng <s.sinthon@cas.com.kh>
2024-09-19 11:03:19 +0700

    new features(api version): configure an api version for minimal api

--- ApiVersioning/Program.cs
+++ ApiVersioning/Program.cs
@@ -1,6 +1,18 @@
+using Asp.Versioning;
+using Asp.Versioning.Builder;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddApiVersioning(options =>
+{
+    options.DefaultApiVersion = new ApiVersion(1, 0);
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.ReportApiVersions = true;
+    options.ApiVersionReader = ApiVersionReader.Combine(
+        new UrlSegmentApiVersionReader(),
+        new HeaderApiVersionReader("X-ApiVersion"));
+}).AddApiExplorer();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -21,5 +33,32 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+// Define the API version set
+ApiVersionSet apiVersionSet = app.NewApiVersionSet()
+    .HasApiVersion(new ApiVersion(1, 0))
+    .HasApiVersion(new ApiVersion(2, 0))
+    .ReportApiVersions()
+    .Build();
+
+// Route group for version 1 and 2
+RouteGroupBuilder versionGroup = app.MapGroup("/api/v{apiVersion:apiVersion}")
+    .WithApiVersionSet(apiVersionSet)
+    .HasApiVersion(1, 0)  // Version 1
+    .HasApiVersion(2, 0); // Version 2
+
+// Endpoint for version 1
+versionGroup.MapGet("/users", () =>
+{
+    return "This is version 1";
+})
+.MapToApiVersion(1, 0);
+
+// Endpoint for version 2
+versionGroup.MapGet("/users", () =>
+{
+    return "This is version 2";
+})
+.MapToApiVersion(2, 0);
 
 app.Run();
